5f84915d858c105c9ff3d85c4ee1fdbd43b785fc
Richard Everett <richard.everett@virgin.net>
2011-09-10 16:40:52 +0100

    Preferences added

---
+++ src/com/richev/planningpokerplusplus/Preferences.java
@@ -0,0 +1,57 @@
+package com.richev.planningpokerplusplus;
+
+import android.content.SharedPreferences;
+
+/**
+ * Wrapper class for strongly-typed access to SharedPreferences
+ * @author Rich
+ *
+ */
+public class Preferences
+{
+    private final SharedPreferences _prefs;
+    
+    /**
+     * Wrapper class for strongly-typed access to SharedPreferences
+     * @param prefs use PreferenceManager.getDefaultSharedPreferences(this)
+     */
+    public Preferences(SharedPreferences prefs)
+    {
+        _prefs = prefs;
+    }
+    
+    public boolean getHideCardOnTap()
+    {
+        return _prefs.getBoolean("hideCardOnTap", true);        
+    }
+    
+    public boolean getIncludeZero()
+    {
+        return _prefs.getBoolean("includeZero", true);
+    }
+
+    public boolean getIncludeHalf()
+    {
+        return _prefs.getBoolean("includeHalf", true);
+    }
+
+    public boolean getIncludeInfinity()
+    {
+        return _prefs.getBoolean("includeInfinity", true);
+    }
+
+    public boolean getIncludeUnknown()
+    {
+        return _prefs.getBoolean("includeUnknown", true);
+    }
+
+    public boolean getIncludeCoffee()
+    {
+        return _prefs.getBoolean("includeCoffee", true);
+    }
+    
+    public String getCardSequenceName()
+    {
+        return _prefs.getString("cardSequence", "fibonacci21");
+    }
+}
